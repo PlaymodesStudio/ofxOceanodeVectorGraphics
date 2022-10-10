5817a284a20c47d8bf14b72ce8411fef4c515313
Eduard Frigola Bagué <eduardfrigola@gmail.com>
2022-10-10 16:44:27 +0200

    Update polyline modules to use path

--- src/ofxOceanodeVectorGraphics.h
+++ src/ofxOceanodeVectorGraphics.h
@@ -22,36 +22,79 @@ public:
         addParameter(x.set("X", {0}, {0}, {1}));
         addParameter(y.set("Y", {0}, {0}, {1}));
         addParameter(w.set("W", {0}, {0}, {20}));
-        addParameter(r.set("R", {0}, {0}, {1}));
-        addParameter(g.set("G", {0}, {0}, {1}));
-        addParameter(b.set("B", {0}, {0}, {1}));
-        addParameter(a.set("A", {0}, {0}, {1}));
+        addParameter(r.set("R", {1}, {0}, {1}));
+        addParameter(g.set("G", {1}, {0}, {1}));
+        addParameter(b.set("B", {1}, {0}, {1}));
+        addParameter(a.set("A", {1}, {0}, {1}));
+		addParameter(fr.set("FillR", {0}, {0}, {1}));
+        addParameter(fg.set("FillG", {0}, {0}, {1}));
+        addParameter(fb.set("FillB", {0}, {0}, {1}));
+        addParameter(fa.set("FillA", {0}, {0}, {1}));
         
-        addParameter(output.set("Output", {ofPolyline()}));
+        addParameter(output.set("Output", {ofPath()}));
     }
     
     void update(ofEventArgs &args){
         bool close = false;
-        vector<glm::vec3> points;
-        if(x->size() == y->size()){
-            points.resize(x->size());
-            for(int i = 0; i < points.size(); i++){
-                points[i] = glm::vec3(x.get()[i], y.get()[i], 0);
-            }
-        }else if(x->size() > y->size()){
-            points.resize(x->size());
-            for(int i = 0; i < points.size(); i++){
-                points[i] = glm::vec3(x.get()[i], y.get()[0], 0);
-            }
-        }else if(x->size() < y->size()){
-            points.resize(y->size());
-            for(int i = 0; i < points.size(); i++){
-                points[i] = glm::vec3(x.get()[0], y.get()[i], 0);
-            }
+        vector<vector<glm::vec3>> points;
+		int index = 0;
+		if(x->size() == y->size()){
+			auto iterbegin = x->begin();
+			auto iterend = std::find(x->begin(), x->end(), -1);
+			int i = 0;
+			while (iterend != x->end()){
+				points.emplace_back();
+				points[i].resize(iterend - iterbegin);
+				int beginIndex = iterbegin - x->begin();
+				for(int j = 0; j < points[i].size(); j++){
+					points[i][j] = glm::vec3(x.get()[beginIndex+j], y.get()[beginIndex+j], 0);
+				}
+				iterbegin = iterend+1;
+				iterend = std::find(iterbegin, x->end(), -1);
+				i++;
+			}
+			points.emplace_back();
+			points[i].resize(iterend - iterbegin);
+			int beginIndex = iterbegin - x->begin();
+			for(int j = 0; j < points[i].size(); j++){
+				points[i][j] = glm::vec3(x.get()[beginIndex+j], y.get()[beginIndex+j], 0);
+			}
+		}else if(x->size() > y->size() && y->size() == 1){
+			points.resize(1);
+			points[0].resize(close ? x->size() + 1 : x->size());
+			for(int i = 0; i < points[0].size(); i++){
+				points[0][i] = glm::vec3(x.get()[i], y.get()[0], 0);
+			}
+		}else if(x->size() < y->size() && x->size() == 1){
+			points.resize(1);
+			points[0].resize(close ? y->size() + 1 : y->size());
+			for(int i = 0; i < points[0].size(); i++){
+				points[0][i] = glm::vec3(x.get()[0], y.get()[i], 0);
+			}
+        }else{
+            return;
         }
+		if(points.back().size() == 0) points.pop_back();
+		
+		auto getFromVec = [](const vector<float> vf, int index) -> float{
+            if(index < vf.size()) return vf[index];
+            return vf[0];
+        };
         		
-		ofPolyline line(points);
-        output.set(vector<ofPolyline>(1, line));
+		vector<ofPath> lines(points.size());
+		
+		for(int i = 0 ; i < points.size(); i++){
+			lines[i].moveTo(points[i][0]);
+			for(int j = 1; j < points[i].size(); j++){
+				lines[i].lineTo(points[i][j]);
+			}
+			lines[i].setStrokeColor(ofFloatColor(getFromVec(r, i), getFromVec(g, i), getFromVec(b, i), getFromVec(a, i)));
+			lines[i].setFillColor(ofFloatColor(getFromVec(fr, i), getFromVec(fg, i), getFromVec(fb, i), getFromVec(fa, i)));
+			lines[i].setStrokeWidth(getFromVec(w, i));
+			
+			//lines[i].
+		}
+        output.set(lines);
     }
     
 private:
@@ -62,7 +105,11 @@ private:
     ofParameter<vector<float>> g;
     ofParameter<vector<float>> b;
     ofParameter<vector<float>> a;
-    ofParameter<vector<ofPolyline>> output;
+    ofParameter<vector<float>> fr;
+    ofParameter<vector<float>> fg;
+    ofParameter<vector<float>> fb;
+    ofParameter<vector<float>> fa;
+    ofParameter<vector<ofPath>> output;
 };
 
 class polylineRenderer : public ofxOceanodeNodeModel{
@@ -70,10 +117,31 @@ public:
     polylineRenderer() : ofxOceanodeNodeModel("Poly Line Renderer"){};
     
     void setup(){
-        addParameter(input.set("Input", {ofPolyline()}));
-        addParameter(width.set("Width", 100, 1, 5000));
-        addParameter(height.set("Height", 100, 1, 5000));
+        auto inputref = addParameter(input.set("Input", {ofPath()}));
+        addParameter(width.set("Width", 100, 1, INT_MAX));
+        addParameter(height.set("Height", 100, 1, INT_MAX));
+        addParameter(texture.set("Tex", nullptr));
         addParameter(output.set("Output", nullptr));
+        
+        inputref->addReceiveFunc<vector<ofxFatLine>>([this](vector<ofxFatLine> const &fl){
+            inputFat.clear();
+            for(auto &f : fl){
+                if(f.getVertices().size() > 1){
+                    ofPath p;
+                    p.moveTo(f.getVertices()[0] / glm::vec3(800, 800, 1));
+                    for(int i = 1 ; i < f.getVertices().size(); i++){
+                        p.lineTo(f.getVertices()[i] / glm::vec3(800, 800, 1));
+                    }
+                    p.setStrokeColor(ofFloatColor(1, 1, 1, 1));
+                    p.setFilled(false);
+                    p.setStrokeWidth(1);
+                    inputFat.push_back(p);
+                }
+            }
+        });
+        inputref->addDisconnectFunc([this](){
+            inputFat.clear();
+        });
     }
     
     void update(ofEventArgs &a){
@@ -87,9 +155,25 @@ public:
         ofClear(0);
 		ofScale(width, height);
         
-        for(auto &l : input.get()){
-			l.draw();//0, 0, width, height);
+        const vector<ofPath> &toDraw = inputFat.size() > 0 ? inputFat : input.get();
+        
+        if(texture.get() != nullptr){
+            texture.get()->bind();
+            for(auto &l : toDraw){
+                ofMesh mesh = l.getTessellation();
+                for(auto & v: mesh.getVertices()){
+                    mesh.addTexCoord(v);//glm::vec2(v.x * texture.get()->getWidth(), v.y * texture.get()->getHeight()));
+                }
+                mesh.draw();
+            }
+            texture.get()->unbind();
+        }else{
+            for(auto &l : toDraw){
+                l.draw();//0, 0, width, height);
+            }
         }
+        
+        inputFat.clear();
         
         ofDisableAlphaBlending();
         ofPopMatrix();
@@ -100,8 +184,10 @@ public:
     
 private:
     ofParameter<int> width, height;
+    ofParameter<ofTexture*> texture;
     ofFbo fbo;
-    ofParameter<vector<ofPolyline>> input;
+    ofParameter<vector<ofPath>> input;
+    vector<ofPath> inputFat;
     ofParameter<ofTexture*> output;
 };
 
