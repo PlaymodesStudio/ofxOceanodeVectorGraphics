49a653a12ed0fdbdb795d14537f9ed82005f26df
Eduard Frigola Bagué <eduardfrigola@gmail.com>
2022-10-14 18:24:47 +0200

    Update svgtools

--- src/svgTools.h
+++ src/svgTools.h
@@ -9,7 +9,7 @@
 #define svgTools_h
 
 #include "ofxOceanodeNodeModel.h"
-//#include "ofxFatLine.h"
+#include "ofxFatLine.h"
 #include "ofxSvg.h"
 
 class svgModulator : public ofxOceanodeNodeModel{
@@ -20,122 +20,88 @@ public:
         addParameter(filename.set("File", ""));
         addParameter(width.set("Width", 100, 1, 5000), ofxOceanodeParameterFlags_DisableSavePreset);
         addParameter(height.set("Height", 100, 1, 5000), ofxOceanodeParameterFlags_DisableSavePreset);
-        addParameter(resample.set("Resample", 100, 2, 1000));
-        addParameter(texModulator.set("TMod", nullptr));
-        addParameter(output.set("Output", {ofxFatLine()}));
+        addParameter(sizes.set("Sizes", {0}, {0}, {INT_MAX}));
+        addOutputParameter(x_out.set("X.Out", {0.5}, {0}, {1}));
+        addOutputParameter(y_out.set("Y.Out", {0.5}, {0}, {1}));
+
         
         listener = filename.newListener([this](string &path){
             svg.load(path);
             int numPaths = svg.getNumPath();
-            ofLog() << numPaths;
-            tempOut.resize(numPaths);
             
             glm::vec2 size(svg.getWidth(), svg.getHeight());
             width = size.x;
             height = size.y;
             
+            vector<float> x_tmp;
+            vector<float> y_tmp;
+            vector<int> sizes_tmp;
             for(int i = 0; i < numPaths; i++){
-                ofPolyline p;
-                for(auto &c : svg.getPathAt(i).getCommands()){
-                    if(c.type == ofPath::Command::moveTo){
-                        p.addVertex(c.to);
-                    }
-                    else if(c.type == ofPath::Command::lineTo){
-                        if(c.to != glm::vec3(0, 0, 0))
-                        p.addVertex(c.to);
-                    }
-                    else if(c.type == ofPath::Command::close){
-                        p.addVertex(p.getVertices()[0]);
-                    }
-                    else{
-                        ofLog() << "unprocessed command";
-                        p.addVertex(c.to);
-                    }
-                }
+//                ofPolyline p;
+//                for(auto &c : svg.getPathAt(i).getCommands()){
+//                    if(c.type == ofPath::Command::moveTo){
+//                        p.addVertex(c.to);
+//                    }
+//                    else if(c.type == ofPath::Command::lineTo){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.lineTo(c.to);
+//                    }
+//                    else if(c.type == ofPath::Command::curveTo){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.curveTo(c.to);
+//                    }
+//                    else if(c.type == ofPath::Command::bezierTo){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.bezierTo(c.to, c.cp1, c.cp2);
+//                    }
+//                    else if(c.type == ofPath::Command::quadBezierTo){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.quadBezierTo(c.to, c.cp1, c.cp2);
+//                    }
+//                    else if(c.type == ofPath::Command::arc){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.arc(c.to, c.);
+//                    }
+//                    else if(c.type == ofPath::Command::arcNegative){
+//                        if(c.to != glm::vec3(0, 0, 0))
+//                        p.addVertex(c.to);
+//                    }
+//                    else if(c.type == ofPath::Command::close){
+//                        p.addVertex(p.getVertices()[0]);
+//                    }
+//                    else{
+//                        ofLog() << "unprocessed command";
+//                        p.addVertex(c.to);
+//                    }
+//                }
 //                if(svg.getPathAt(i).isClosed())
 //                    tempOut[i].addVertex(tempOut[i].getVertices()[0]);
-                //p = p.getResampledByCount(100);
-                //while(p.size() > 100) p.removeVertex(p.size()-1);
-                tempOut[i].setGlobalWidth(10);
-                tempOut[i].setFromPolyline(p);
+                
+                
+                for(auto &v : svg.getPathAt(i).getOutline()){
+                    for(auto &p : v){
+                        x_tmp.push_back(p.x);
+                        y_tmp.push_back(p.y);
+                    }
+                    sizes_tmp.push_back(v.size());
+                    x_tmp.push_back(-1);
+                    y_tmp.push_back(-1);
+                }
             }
-            output = tempOut;
-            
-//            ofPolyline p;
-//            for(auto &c : svg.getPathAt(0).getCommands()){
-//                if(c.to != glm::vec3(0, 0, 0))
-//                    p.addVertex(c.to / size);
-//            }
-//            p.addVertex(p.getVertices()[0]);
-//            resample = p.getVertices().size();
-////            p = p.getResampledByCount(100);
-////            while(p.size() > 100) p.removeVertex(p.size()-1);
-//            output = p;
-        });
-        
-        
-        listener2 = resample.newListener([this](int &i){
-//            ofPolyline p;
-//            p = output->getResampledByCount(i);
-//            while(p.size() > i) p.removeVertex(p.size()-1);
-//            output = p;
+            sizes = sizes_tmp;
+            y_out = y_tmp;
+            x_out = x_tmp;
         });
     }
     
-    void update(ofEventArgs &a){
-    //        if(modulator->size() == 100){
-    //            for (int i = 0; i < 100 ; i++){
-    //                auto c = ofFloatColor(modulator->at(i));
-    //                for (int j = 0; j < svg.getNumPath() ; j++)
-    //                    tempOut[j].updateColor(i, c);
-    //            }
-    //        }
-            
-            if(texModulator.get() != nullptr){
-                ofFloatPixels pixels;
-                texModulator.get()->readToPixels(pixels);
-                int texWidth = texModulator.get()->getWidth();
-                size_t texHeight = texModulator.get()->getHeight();
-                float *p = pixels.begin();
-                int pixelSize = pixels.getPixelFormat();
-    //            for(int i = 0; i < texWidth; i++){
-    //                for(int j = 0; j < texHeigh; j++){
-    //                    int index = ((j*texWidth)+(i))*pixelSize;
-    //                    ofFloatColor c;
-    //                    c.r = p[index];
-    //                    c.g = p[index+1];
-    //                    c.b = p[index+2];
-    //                    tempOut[j].updateColor(i, c);
-    //                }
-    //            }
-                for(int j = 0; j < min(tempOut.size(), texHeight); j++){
-                    int lineWidth = tempOut[j].size();//sizes.get()[j];
-                    ofFloatColor c;
-                    for(int i = 0; i < lineWidth; i++){
-                        int index = ((j*texWidth)+(i))*pixelSize;
-                        c.r = p[index];
-                        c.g = p[index+1];
-                        c.b = p[index+2];
-                        tempOut[j].updateColor(i, c);
-                    }
-                    //tempOut[j].setColor(vc);
-                }
-            }
-            output = tempOut;
-        }
-    
 private:
     ofEventListener listener;
-    ofEventListener listener2;
     
     ofxSVG svg;
-    vector<ofxFatLine> tempOut;
     ofParameter<int> width, height;
     ofParameter<string> filename;
-    ofParameter<int>    resample;
-    ofParameter<ofTexture*> texModulator;
-    //ofParameter<vector<float>> modulator;
-    ofParameter<vector<ofxFatLine>> output;
+    ofParameter<vector<float>> x_out, y_out;
+    ofParameter<vector<int>> sizes;
 };
 
 #endif /* svgTools_h */
