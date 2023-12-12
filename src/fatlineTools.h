e7a04df7fcb235e901383332a2d4c9a2cbba8bd0
eloimaduell <eloi@playmodes.com>
2023-12-12 20:32:50 +0100

    updated "Manual Points" with keep aspect ratio. Changed Input texture by Background. Added Origin position toggle (top left or bottom left)

--- src/fatlineTools.h
+++ src/fatlineTools.h
@@ -86,6 +86,7 @@ public:
 		//if(points.back().size() == 0) points.pop_back();
         
         auto getFromVec = [](const vector<float> vf, int index) -> float{
+            if(vf.size()==0) return -1;
             if(index < vf.size()) return vf[index];
             return vf[0];
         };
@@ -187,34 +188,37 @@ private:
 
 class manualPoints : public ofxOceanodeNodeModel{
 public:
-    manualPoints() : ofxOceanodeNodeModel("Manual Points"){}
+    manualPoints() : ofxOceanodeNodeModel("Manual Points"){
+        description = "Interactive window opens where you can create points and drag them around a [0,0] to [1,1] space.\nRight-Click to create or delete points.\nLeft-click to drag points.\nKeyboard arrows move the selected point. (+alt moves very slowly).\nTAB key switches between selected points.\n\nInput is a texture to apply as background to the window area.";
+    }
+    
+    void updatePoints()
+    {
+        if(x->size() == y->size() && !outputingValues){
+            points.clear();
+            points.resize(x->size());
+            for(int i = 0; i < points.size(); i++){
+                    points[i] == glm::vec2(x->at(i), y->at(i));
+            }
+        }
+    }
     
     void setup(){
-        addParameter(input.set("Input", nullptr));
-		addParameter(x.set("X", {0.5}, {0}, {1}));
-        addParameter(y.set("Y", {0.5}, {0}, {1}));
-        addParameter(mouseX.set("M.x", 0.5, 0, 1));
-        addParameter(mouseY.set("M.y", 0.5, 0, 1));
-		addParameter(numPoints.set("Num", {1}, {1}, {INT_MAX}));
-        
+        addParameter(showWindow.set("Show",false));
+        addParameter(originLow.set("OrigPos",false));
+        addParameter(input.set("Background", nullptr));
+        addOutputParameter(mouseX.set("M.x", 0.5, 0, 1));
+        addOutputParameter(mouseY.set("M.y", 0.5, 0, 1));
+        addOutputParameter(numPoints.set("Num", {1}, {1}, {INT_MAX}));
+        addOutputParameter(x.set("X", {0.5}, {0}, {1}));
+        addOutputParameter(y.set("Y", {0.5}, {0}, {1}));
+
         listeners.push(x.newListener([this](vector<float> &vf){
-            if(x->size() == y->size() && !outputingValues){
-                points.clear();
-                points.resize(x->size());
-                for(int i = 0; i < points.size(); i++){
-                    points[i] == glm::vec2(x->at(i), y->at(i));
-                }
-            }
+            updatePoints();
         }));
         
         listeners.push(y.newListener([this](vector<float> &vf){
-            if(x->size() == y->size() && !outputingValues){
-                points.clear();
-                points.resize(x->size());
-                for(int i = 0; i < points.size(); i++){
-                    points[i] == glm::vec2(x->at(i), y->at(i));
-                }
-            }
+            //updatePoints();
         }));
 		
 		points.emplace_back(0.5, 0.5);
@@ -231,100 +235,156 @@ public:
 		black.loadData(pix);
     }
     
-    void draw(ofEventArgs &a){
-        if(ImGui::Begin(("Manual Points " + ofToString(getNumIdentifier())).c_str())){
-            auto screenSize = ImGui::GetContentRegionAvail();
-            auto screenPos = ImGui::GetCursorScreenPos();
-            ImTextureID textureID = (ImTextureID)(uintptr_t)black.texData.textureID;
-            if(input.get() != nullptr){
-                textureID = (ImTextureID)(uintptr_t)input.get()->texData.textureID;
-            }
-            ImGui::Image(textureID, screenSize);
-			if(ImGui::IsWindowHovered()){
-				glm::vec2 normPos = (ImGui::GetMousePos() - screenPos) / screenSize;
-				bool addRemove = false;
-                bool mouseClicked = false;
-                if(ImGui::IsMouseClicked(0)){
-                    mouseClicked = true;
-                }
-                else if(ImGui::IsMouseClicked(1)){
-                    mouseClicked = true;
-					addRemove = true;
-                }
-				if(mouseClicked){
-					bool foundPoint = false;
-					for(int i = points.size()-1; i >= 0 && !foundPoint ; i--){
-						auto point = (points[i] * screenSize) + screenPos;
-						if(glm::distance(glm::vec2(ImGui::GetMousePos()), point) < 10){
-							pointDraggingIndex = i;
-							foundPoint = true;
-						}
-					}
-					if(ImGui::GetIO().KeyShift){
-						points.clear();
-						points.push_back(normPos);
-					}
-					else if(addRemove){
-						if(foundPoint){
-							if(points.size() > 1){
-								points.erase(points.begin() + pointDraggingIndex);
-							}
-							pointDraggingIndex = -1;
-						}else{
-							points.push_back(normPos);
-                            pointDraggingIndex = points.size()-1;
-						}
-                    }else if(!foundPoint){
-                        pointDraggingIndex = -1;
+    void draw(ofEventArgs &a) override{
+        if(showWindow)
+        {
+            if(ImGui::Begin(("Manual Points " + ofToString(getNumIdentifier())).c_str())){
+                if(ImGui::Button("[Reset Points]"))
+                {
+                    for(int i=0;i<x.get().size();i++)
+                    {
+                        points[i]=glm::vec2(0.5,0.5);
                     }
-				}
-				else if(ImGui::IsMouseDragging(0)){
-					if(pointDraggingIndex != -1){
-						if(ImGui::GetIO().KeyAlt){
-                            points[pointDraggingIndex] += ImGui::GetIO().MouseDelta / (screenSize * ImVec2(100, 100));
-						}else{
-                            points[pointDraggingIndex] += ImGui::GetIO().MouseDelta / screenSize;
+                }
+                ImGui::SameLine();
+                if(keepAspectRatio)ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0,0.5,0.0,1.0));
+                else ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.55,0.55,0.55,1.0));
+                if(ImGui::Button("[AR]"))
+                {
+                    if(ImGui::IsMouseReleased(0)) keepAspectRatio=!keepAspectRatio;
+                }
+                ImGui::PopStyleColor();
+                
+                auto screenSize = ImGui::GetContentRegionAvail();
+                if(keepAspectRatio)
+                {
+                    ImVec2 sizeAux = screenSize;
+                    if(screenSize.y>screenSize.x) sizeAux.y = sizeAux.x;
+                    else sizeAux.x = screenSize.y;
+                    screenSize=sizeAux;
+                }
+                
+                auto screenPos = ImGui::GetCursorScreenPos();
+                //ImGui::Vec2 size;
+                
+                ImTextureID textureID = (ImTextureID)(uintptr_t)black.texData.textureID;
+                if(input.get() != nullptr){
+                    textureID = (ImTextureID)(uintptr_t)input.get()->texData.textureID;
+                }
+                ImGui::Image(textureID, screenSize);
+                if(ImGui::IsWindowHovered()){
+                    glm::vec2 normPos = (ImGui::GetMousePos() - screenPos) / screenSize;
+                    if(normPos.x>1.0) normPos.x = 1.0;
+                    if(normPos.x<0.0) normPos.x = 0.0;
+                    if(normPos.y>1.0) normPos.y = 1.0;
+                    if(normPos.y<0.0) normPos.y = 0.0;
+
+                    bool addRemove = false;
+                    bool mouseClicked = false;
+                    if(ImGui::IsMouseClicked(0)){
+                        mouseClicked = true;
+                    }
+                    else if(ImGui::IsMouseClicked(1)){
+                        mouseClicked = true;
+                        addRemove = true;
+                    }
+                    if(mouseClicked){
+                        bool foundPoint = false;
+                        for(int i = points.size()-1; i >= 0 && !foundPoint ; i--){
+                            auto point = (points[i] * screenSize) + screenPos;
+                            if(glm::distance(glm::vec2(ImGui::GetMousePos()), point) < 10){
+                                pointDraggingIndex = i;
+                                foundPoint = true;
+                            }
                         }
-					}else if(ImGui::GetIO().KeyShift){
-						points.push_back((ImGui::GetMousePos() - screenPos) / screenSize);
-					}
-                }else if(pointDraggingIndex != -1){
-                    float moveAmt = ImGui::GetIO().KeyAlt ? 0.00001 : 0.001;
-                    if(ImGui::IsKeyDown(ImGuiKey_LeftArrow)){
-                        points[pointDraggingIndex] += glm::vec2(-moveAmt, 0);
-                    }else if(ImGui::IsKeyDown(ImGuiKey_RightArrow)){
-                        points[pointDraggingIndex] += glm::vec2(moveAmt, 0);
-                    }if(ImGui::IsKeyDown(ImGuiKey_UpArrow)){
-                        points[pointDraggingIndex] += glm::vec2(0, -moveAmt);
-                    }else if(ImGui::IsKeyDown(ImGuiKey_DownArrow)){
-                        points[pointDraggingIndex] += glm::vec2(0, moveAmt);
+                        if(ImGui::GetIO().KeyShift){
+                            points.clear();
+                            points.push_back(normPos);
+                        }
+                        else if(addRemove){
+                            if(foundPoint){
+                                if(points.size() > 1){
+                                    points.erase(points.begin() + pointDraggingIndex);
+                                }
+                                pointDraggingIndex = -1;
+                            }else{
+                                points.push_back(normPos);
+                                pointDraggingIndex = points.size()-1;
+                            }
+                        }else if(!foundPoint){
+                            pointDraggingIndex = -1;
+                        }
+                    }
+                    else if(ImGui::IsMouseDragging(0)){
+                        if(pointDraggingIndex != -1){
+                            if(ImGui::GetIO().KeyAlt){
+                                points[pointDraggingIndex] += ImGui::GetIO().MouseDelta / (screenSize * ImVec2(100, 100));
+                            }else{
+                                points[pointDraggingIndex] += ImGui::GetIO().MouseDelta / screenSize;
+                            }
+                        }else if(ImGui::GetIO().KeyShift){
+                            points.push_back((ImGui::GetMousePos() - screenPos) / screenSize);
+                        }
+                    }else if(pointDraggingIndex != -1){
+                        float moveAmt = ImGui::GetIO().KeyAlt ? 0.00001 : 0.001;
+                        if(ImGui::IsKeyDown(ImGuiKey_LeftArrow)){
+                            points[pointDraggingIndex] += glm::vec2(-moveAmt, 0);
+                        }else if(ImGui::IsKeyDown(ImGuiKey_RightArrow)){
+                            points[pointDraggingIndex] += glm::vec2(moveAmt, 0);
+                        }if(ImGui::IsKeyDown(ImGuiKey_UpArrow)){
+                            points[pointDraggingIndex] += glm::vec2(0, -moveAmt);
+                        }else if(ImGui::IsKeyDown(ImGuiKey_DownArrow)){
+                            points[pointDraggingIndex] += glm::vec2(0, moveAmt);
+                        }
+                    }
+                    if(ImGui::IsKeyPressed(ImGuiKey_Tab)){
+                        pointDraggingIndex = (pointDraggingIndex + 1) % points.size();
+                    }
+                    
+                    if(points[pointDraggingIndex].x>1.0) points[pointDraggingIndex].x=1.0;
+                    if(points[pointDraggingIndex].x<0.0) points[pointDraggingIndex].x=0.0;
+                    if(points[pointDraggingIndex].y>1.0) points[pointDraggingIndex].y=1.0;
+                    if(points[pointDraggingIndex].y<0.0) points[pointDraggingIndex].y=0.0;
+                    
+                    mouseY = normPos.y;
+                    mouseX = normPos.x;
+                }
+                ImDrawList* draw_list = ImGui::GetWindowDrawList();
+                vector<float> x_t(points.size());
+                vector<float> y_t(points.size());
+                for(int i = 0; i < points.size(); i++){
+                    if(pointDraggingIndex == i) draw_list->AddCircle((points[i] * screenSize) + screenPos, 10, IM_COL32(255, 255, 0, 255));
+                    else draw_list->AddCircle((points[i] * screenSize) + screenPos, 10, IM_COL32(255, 128, 0, 255));
+                    string numString = ofToString(i);
+                    draw_list->AddText(ImGui::GetFont(), ImGui::GetFontSize(), (points[i] * screenSize) + screenPos - glm::vec2(5, 5), IM_COL32(255,255,255,255), numString.c_str(), numString.c_str()+ (i < 10 ? 1 : 2));
+                    x_t[i] = points[i].x;
+                    if(!originLow)
+                    {
+                        y_t[i] = points[i].y;
+                    }
+                    else
+                    {
+                        y_t[i] = -1*(points[i].y-1.0);
                     }
                 }
-                if(ImGui::IsKeyPressed(ImGuiKey_Tab)){
-                    pointDraggingIndex = (pointDraggingIndex + 1) % points.size();
+                if(!originLow)
+                {
+                    draw_list->AddCircleFilled(screenPos,5, IM_COL32(255, 128, 0, 128));
                 }
-                mouseY = normPos.y;
-                mouseX = normPos.x;
+                else
+                {
+                    draw_list->AddCircleFilled(screenPos+ImVec2(0,screenSize.y),5, IM_COL32(255, 128, 0, 128));
+                }
+                draw_list->ChannelsMerge();
+                outputingValues = true;
+                y = y_t;
+                x = x_t;
+                outputingValues = false;
+                numPoints = points.size();
             }
-			ImDrawList* draw_list = ImGui::GetWindowDrawList();
-			vector<float> x_t(points.size());
-			vector<float> y_t(points.size());
-			for(int i = 0; i < points.size(); i++){
-                if(pointDraggingIndex == i) draw_list->AddCircleFilled((points[i] * screenSize) + screenPos, 10, IM_COL32(255, 255, 0, 255));
-				else draw_list->AddCircleFilled((points[i] * screenSize) + screenPos, 10, IM_COL32(255, 255, 255, 255));
-				string numString = ofToString(i);
-				draw_list->AddText(ImGui::GetFont(), ImGui::GetFontSize(), (points[i] * screenSize) + screenPos - glm::vec2(5, 5), IM_COL32(0,0,0,255), numString.c_str(), numString.c_str()+ (i < 10 ? 1 : 2));
-				x_t[i] = points[i].x;
-				y_t[i] = points[i].y;
-			}
-			draw_list->ChannelsMerge();
-            outputingValues = true;
-            y = y_t;
-			x = x_t;
-            outputingValues = false;
-			numPoints = points.size();
+            ImGui::End();
+
         }
-        ImGui::End();
     }
 	
 	void presetSave(ofJson &json) override{
@@ -354,12 +414,15 @@ private:
     ofParameter<vector<float>> x, y;
     ofParameter<float> mouseX, mouseY;
 	ofParameter<int> numPoints;
+    ofParameter<bool> originLow;
+    ofParameter<bool> showWindow;
 	vector<glm::vec2> points;
     
     ofEventListeners listeners;
 	
 	int pointDraggingIndex = -1;
     bool outputingValues = false;
+    bool keepAspectRatio = true;
 };
 
 class pointsResamplerIndex : public ofxOceanodeNodeModel {
