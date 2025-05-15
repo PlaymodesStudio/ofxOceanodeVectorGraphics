fcd90a5b7f4b2eca500e7b3fa664da6e2b189487
eloimaduell <eloi@playmodes.com>
2025-05-16 01:34:22 +0200

    [Dot Maker] : Bring it to Update for a case when traversing graphs where we end up having no input coordinates into this node. If no input then there was no processing of the event and generation of the output, bringing us to keep the last coordinates when there is no data available to draw.

--- src/fatlineTools.h
+++ src/fatlineTools.h
@@ -623,31 +623,39 @@ public:
         addParameter(y_out.set("Y_out", {0.5}, {0}, {1}));
 		
 		listeners.push(x.newListener([this](vector<float> &vf){
-			if(x->size() == y->size()){
-				x_tmp.clear();
-				y_tmp.clear();
-				x_tmp.reserve((x->size()*2));
-				y_tmp.reserve((x->size()*2));
-				
-				for(int i = 0; i < x->size(); i++){
+			//generateDots();
+		}));
+		
+	}
+	void generateDots()
+	{
+		if(x->size() == y->size()){
+			x_tmp.clear();
+			y_tmp.clear();
+			x_tmp.reserve((x->size()*2));
+			y_tmp.reserve((x->size()*2));
+			
+			for(int i = 0; i < x->size(); i++){
 //					x_tmp[(i*2)] = x->at(i);
 //					y_tmp[(i*2)] = y->at(i);
 //
 //					x_tmp[(i*2)+1] = -1;
 //					y_tmp[(i*2)+1] = -1;
-					if(x->at(i) != -1){
-						x_tmp.push_back(x->at(i));
-						y_tmp.push_back(y->at(i));
-						
-						x_tmp.push_back(-1);
-						y_tmp.push_back(-1);
-					}
+				if(x->at(i) != -1){
+					x_tmp.push_back(x->at(i));
+					y_tmp.push_back(y->at(i));
+					
+					x_tmp.push_back(-1);
+					y_tmp.push_back(-1);
 				}
-                y_out = y_tmp;
-				x_out = x_tmp;
 			}
-		}));
-		
+			y_out = y_tmp;
+			x_out = x_tmp;
+		}
+	}
+	
+	void update(ofEventArgs &a) override {
+		generateDots();
 	}
 	
 private:
@@ -655,6 +663,7 @@ private:
 	vector<float> x_tmp, y_tmp;
 	ofParameter<vector<float>> x_out, y_out;
 
+	
 	ofEventListeners listeners;
 };
 
